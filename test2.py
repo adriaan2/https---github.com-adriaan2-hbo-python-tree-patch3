1b955c1af47a3aa834b871d11767aea98156a89c
adriaan2 <adriaan.kovac@gmail.com>
2022-11-02 13:07:18 +0100

    test

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
 
 
-i =False
-def checkpassword():
-    while i==False:
-        password=input("getpassword")
-        if len(password)>7 and len(password)<21:
-            print("test")
-                
-        else :
-            print("invalid password")
-if __name__=="__main__":
-    checkpassword()
+def isPalindrome(s):
+    return s == s[::-1]
+ 
+ 
+# Driver code
+s = input()
+ans = isPalindrome(s)
+ 
+if ans:
+    print("is a  palidrome")
+else:
+    print("is not a palidrome")
