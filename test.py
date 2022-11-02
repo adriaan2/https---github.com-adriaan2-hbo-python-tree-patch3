442a19effbbe05066b7e702be96c00d377fb50cd
adriaan2 <adriaan.kovac@gmail.com>
2022-11-02 14:44:24 -0700

    test

--- test.py
+++ test.py
@@ -1,22 +1,9 @@
-string="abcderfgtreergrefefrgtr"
-Hashmap = list(string)
+
+hashmap_key_value={}
+key=input("an even number of letters")
+i=0 
+while i<len(key):
+    hashmap_key_value.update({key[i]:key[i+1]})
+    i+=2
     
-    keys = []
-    values = []
-    i = 0
-    #
-    while (i < len(Hashmap)):
-        if i % 2 == 0:
-            keys.append(Hashmap[i])
-        else:
-            values.append(Hashmap[i])
-            i += 1
-    
-    Hashdict = dict()
-    j = 0
-    while j < len(keys):
-        if (j < len(values)):
-            Hashdict.update({keys[j]: values[j]})
-        else:
-            Hashdict.update({keys[j]: None})
-        j += 1
+print(hashmap_key_value)
