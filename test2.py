68c7482b4eeb6751acedbb3d5ead95ecdf6f34d4
adriaan2 <adriaan.kovac@gmail.com>
2022-11-09 11:13:34 +0100

    test

--- test2.py
+++ test2.py
@@ -6,10 +6,16 @@ while loop<x:
     password=input("password")
     xt=len(password)
     passet=set(password)
+    for i in passet:
+      if i.isupper():
+        print("test")
+      if i.isnumeric():
+        print("num")
+      if xt>7 and xt<21:
+        print("size")
+        i+=len(passet)
         
-    print("invalid password")
-    loop+=1
-            
+    
 
 
             
