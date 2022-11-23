a9ca4060b38380808c257290493d70ed45f9564c
adriaan2 <adriaan.kovac@gmail.com>
2022-11-23 10:52:01 +0100

    test

---
+++ a2.py
@@ -0,0 +1,16 @@
+while True :
+    awnser=input("Y/N ").upper()
+    print(awnser)
+    if awnser=="Y":
+        while True:
+            awnser2=input("Asccepted of rejected ").capitalize()
+            if awnser2=="Accepted":
+                while True:
+                    firstname=input()
+                    if len(firstname)>2:
+                        print("test")
+                    else:
+                        print("test")
+        
+                        break
+            else:
